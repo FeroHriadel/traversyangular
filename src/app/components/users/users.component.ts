a6936770d96f50bd1be741bf5940a15dd198613a
Ferdinand Hriadel <ferdinand.hriadel@dxc.com>
2022-04-19 09:43:22 +0200

    intro notes

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -15,6 +15,9 @@ export class UsersComponent implements OnInit {
   users: User[] = [];
   showExtended: boolean = true;
   loaded: boolean = false;
+  enableAdd: boolean = true;
+  currentClasses = {};
+  currentStyles = {};
 
   constructor() {}
 
@@ -29,7 +32,11 @@ export class UsersComponent implements OnInit {
               state: 'state',
               city: 'city',
               street: 'street'
-          }
+          },
+          image: 'https://source.unsplash.com/random',
+          isActive: true,
+          balance: 100,
+          registered: new Date('01/02/2018 08:30:00')
         },
         {
           firstName: 'Betty',
@@ -39,7 +46,10 @@ export class UsersComponent implements OnInit {
               state: 'state',
               city: 'city',
               street: 'street'
-          }
+          },
+          image: 'https://source.unsplash.com/random',
+          balance: 200,
+          registered: new Date('03/11/2017 06:20:00')
         },
         {
           firstName: 'Cecil',
@@ -49,16 +59,35 @@ export class UsersComponent implements OnInit {
               state: 'state',
               city: 'city',
               street: 'street'
-          }
+          },
+          image: 'https://source.unsplash.com/random',
+          balance: 300,
+          registered: new Date('01/02/2018 08:30:00')
         }
       ];
 
       this.loaded = true;
     }, 2000);
+
+    this.setCurrentClasses();
+    this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.users.push(user);
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.enableAdd
+    }
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '30px'
+    }
   }
 
 }
